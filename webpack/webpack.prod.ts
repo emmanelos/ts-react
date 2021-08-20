d30687ba6e34e72b8d21bfbce599a55cf259e5b0
emmanelos <emmanelosg@gmail.com>
2021-08-20 18:11:12 -0400

    Update webpack.prod.ts

--- webpack/webpack.prod.ts
+++ webpack/webpack.prod.ts
@@ -1,14 +1,12 @@
 import { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
 import { webpackCommon } from './webpack.common';
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-import TerserPlugin from 'terser-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
 import { GenerateSW } from 'workbox-webpack-plugin';
 import * as path from 'path';
-const { ESBuildMinifyPlugin } = require('esbuild-loader');
+import { ESBuildMinifyPlugin } from 'esbuild-loader';
 
 exports.default = merge<Configuration>(webpackCommon, {
 	mode: 'production',
@@ -59,7 +57,9 @@ exports.default = merge<Configuration>(webpackCommon, {
 				{ from: 'public/logo512.png', to: 'assets/img' },
 			],
 		}),
+		//@ts-ignore
 		new CleanWebpackPlugin(),
+		//@ts-ignore
 		new GenerateSW({
 			sourcemap: false,
 			swDest: 'service-worker.js',
